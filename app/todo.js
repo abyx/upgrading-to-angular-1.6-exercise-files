874f9575663515cb984f163dbf0f25df16d56763
Aviv Ben-Yosef <aviv.by@gmail.com>
2017-01-01 22:29:01 +0200

    02_04_ScopeWatchAndCtrlAs

--- app/todo.js
+++ app/todo.js
@@ -2,14 +2,14 @@ angular.module('app').controller('TodoCtrl', function($scope, $timeout) {
   var wayToGoPromise = undefined;
   var self = this;
 
-  $scope.$watch('todo', function() {
+  $scope.$watch(function() { return self.todo; }, function() {
     var name = self.todo.name;
     self.formattedTodoName = name.charAt(0).toUpperCase()
         + name.substring(1).toLowerCase();
   });
 
-  $scope.$watch('todo.done', displayWayToGoMessage);
-
+  $scope.$watch(function() { return self.todo.done; }, displayWayToGoMessage);
+  
   function displayWayToGoMessage() {
     if (self.todo.done) {
       self.showWayToGo = true;
