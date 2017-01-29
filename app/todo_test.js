f09d7b4fb1d9cc04d32027e2d10ef4636ef1310e
Aviv Ben-Yosef <aviv.by@gmail.com>
2017-01-29 16:58:12 +0200

    06_01_componentController

--- app/todo_test.js
+++ app/todo_test.js
@@ -1,26 +1,17 @@
 describe('todo', function() {
   beforeEach(module('app'));
 
-  describe('todo controller', function() {
-    var todoCtrl;
-    var scope;
-
-    beforeEach(inject(function($controller, $rootScope) {
-      scope = $rootScope.$new();
-
-      todoCtrl = $controller('TodoCtrl', {$scope: scope});
-    }));
-
-
-    it('should format name', inject(function($controller, $rootScope) {
-      scope.todo = {
+  describe('todo component', function() {
+    it('should format name', inject(function($componentController) {
+      var todo = {
         name: 'name',
         done: false
       };
 
-      scope.$digest();
+      var todoCtrl = $componentController('todo', {}, {todo: todo});
+      todoCtrl.$onInit();
 
-      expect(scope.formattedTodoName).toBe('Name');
+      expect(todoCtrl.formattedTodoName).toBe('Name');
     }));
   });
 });
